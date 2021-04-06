9d2d9b337ca13a14fdb902f4a4b21d2a3452a880
Logan Donley <ldonley@cloudbees.com>
2021-04-06 14:49:39 -0400

    Adding --wait for agent install

--- run.sh
+++ run.sh
@@ -100,5 +100,6 @@ if [ "$CD_ENABLED" = true ]; then
   # Install CD agent
   helm upgrade --install agent cloudbees/cloudbees-flow-agent \
     -f cd/agent/values.yaml -n "$CD_NAMESPACE" \
-    --set flowCredentials.password=$CD_ADMIN_PASS
+    --set flowCredentials.password=$CD_ADMIN_PASS \
+    --wait
 fi
